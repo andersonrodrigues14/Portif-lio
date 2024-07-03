e3591bc168fb132e1254f93e2e04c96226e6c084
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2024-07-03 00:12:25 -0300

    Criados components de botão e tech-badge

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,8 +1,31 @@
+import { Button } from "@/app/components/button";
+import { TechBadge } from "@/app/components/tech-badge";
 import Image from "next/image";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { TbBrandGithub, TbBrandLinkedin, TbBrandYoutube, TbBrandWhatsapp } from "react-icons/tb";
+
+const MOCK_CONTACTS = [
+    {
+        url: 'https://github.com.br',
+        icon: <TbBrandGithub />
+    },
+    {
+        url: 'https://github.com.br',
+        icon: <TbBrandLinkedin />
+    },
+    {
+        url: 'https://github.com.br',
+        icon: <TbBrandYoutube />
+    },
+    {
+        url: 'https://github.com.br',
+        icon: <TbBrandWhatsapp />
+    },
+]
 
 export const HeroSection = () => {
     return (
-        <section className="w-full h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
+        <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
             <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
                 <div className="w-full lg:max-w-[530px]">
                     <p className="font-mono text-emerald-400">Olá, meu nome é</p>
@@ -13,12 +36,32 @@ export const HeroSection = () => {
                         Olá, meu nome é Anderson Almeida Rodrigues e sou desenvolvedor full-stack apaixonado por tecnologia. Com mais de 2 anos de experiência. Meu objetivo é criar sistemas de ponta a ponta funcionais, sempre disposto a embarcar em projetos desafiadores. Estou sempre aberto a novas oportunidade e desafios.
                     </p>
                     
-                    <div>
-                        techs
+                    <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]">
+                        {Array.from({length: 7}).map(( _, index ) => (
+                            <TechBadge 
+                                name="Django"
+                            />
+                        ))}
                     </div>
 
-                    <div>
-                        Contato
+                    <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row">
+                        <Button className="w-max shadow-button">
+                            Entre em contato
+                            <HiArrowNarrowRight size={18} />
+                        </Button>
+
+                        <div className="text-2xl text-gray-600 flex items-center h-20 gap-3">
+                            {MOCK_CONTACTS.map( (contact, index) => (
+                                <a 
+                                    href={contact.url}
+                                    key={`contact-${index}`}
+                                    target="_blanck"
+                                    className="hover:text-gray-100 transition-colors"
+                                >
+                                    {contact.icon}
+                                </a>
+                            ))}
+                        </div>
                     </div>
                 </div>
 
@@ -27,6 +70,7 @@ export const HeroSection = () => {
                     height={404}
                     src="/images/profile-pic.png"
                     alt="Foto de perfil do Anderson Rodrigues"
+                    className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover"
                 />
             </div>
 
