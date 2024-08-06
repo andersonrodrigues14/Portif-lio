f9aebea9c7ae9b326426757b7f0d06c9acec1359
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2024-08-06 19:00:26 -0300

    Ajuste na altura do card

--- app/components/pages/home/known-techs/known-tech.tsx
+++ app/components/pages/home/known-techs/known-tech.tsx
@@ -49,7 +49,7 @@ export const KnowTech = ({tech}:KnowTechProps) => {
     }
     
     return (
-        <div className="p-6 rounded-lg bg-gray-600/20 text-gray-500 flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
+        <div className="h-32 p-6 rounded-lg bg-gray-600/20 text-gray-500 flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all">
             <div className="flex items-center justify-between">
                 <p className="font-medium">{tech.name}</p>
                 <CMSIcon icon={tech.iconSvg}/>
